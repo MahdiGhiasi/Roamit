9f18e11e8db843e9892be357916c600ecef7b4d3
Mahdi Ghiasi <Mahdi Ghiasi>
2017-04-06 00:04:06 +0430

    CircularProgressBar

---
+++ QuickShare/Controls/Arc.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+//From http://blog.jerrynixon.com/2012/06/windows-8-animated-pie-slice.html
+//With modifications
+
+namespace QuickShare.Controls
+{
+    public sealed class Arc : Path
+    {
+        private bool m_HasLoaded = false;
+        public Arc()
+        {
+            Loaded += (s, e) =>
+            {
+                m_HasLoaded = true;
+                UpdatePath();
+            };
+        }
+
+        // StartAngle
+        public static readonly DependencyProperty StartAngleProperty
+            = DependencyProperty.Register("StartAngle", typeof(double), typeof(Arc),
+            new PropertyMetadata(DependencyProperty.UnsetValue, (s, e) => { Changed(s as Arc); }));
+        public double StartAngle
+        {
+            get { return (double)GetValue(StartAngleProperty); }
+            set { SetValue(StartAngleProperty, value); }
+        }
+
+        // Angle
+        public static readonly DependencyProperty AngleProperty
+            = DependencyProperty.Register("Angle", typeof(double), typeof(Arc),
+            new PropertyMetadata(DependencyProperty.UnsetValue, (s, e) => { Changed(s as Arc); }));
+        public double Angle
+        {
+            get { return (double)GetValue(AngleProperty); }
+            set { SetValue(AngleProperty, value); }
+        }
+
+        // Radius
+        public static readonly DependencyProperty RadiusProperty
+            = DependencyProperty.Register("Radius", typeof(double), typeof(Arc),
+            new PropertyMetadata(DependencyProperty.UnsetValue, (s, e) => { Changed(s as Arc); }));
+        public double Radius
+        {
+            get { return (double)GetValue(RadiusProperty); }
+            set { SetValue(RadiusProperty, value); }
+        }
+
+        private static void Changed(Arc pieSlice)
+        {
+            if (pieSlice.m_HasLoaded)
+                pieSlice.UpdatePath();
+        }
+
+        public void UpdatePath()
+        {
+            // ensure variables
+            if (GetValue(StartAngleProperty) == DependencyProperty.UnsetValue)
+                throw new ArgumentNullException("Start Angle is required");
+            if (GetValue(RadiusProperty) == DependencyProperty.UnsetValue)
+                throw new ArgumentNullException("Radius is required");
+            if (GetValue(AngleProperty) == DependencyProperty.UnsetValue)
+                throw new ArgumentNullException("Angle is required");
+
+            Width = Height = 2 * (Radius + StrokeThickness);
+            var _EndAngle = StartAngle + Angle;
+
+            // path container
+            var _StartX = Radius + Math.Sin(StartAngle * Math.PI / 180) * Radius;
+            var _StartY = Radius - Math.Cos(StartAngle * Math.PI / 180) * Radius;
+            var _StartP = new Point(_StartX, _StartY);
+            var _Figure = new PathFigure
+            {
+                StartPoint = _StartP,
+                IsClosed = false,
+            };
+
+            // outer arc
+            var _ArcX = Radius + Math.Sin(_EndAngle * Math.PI / 180) * Radius;
+            var _ArcY = Radius - Math.Cos(_EndAngle * Math.PI / 180) * Radius;
+            var _ArcS = new Size(Radius, Radius);
+            var _ArcP = new Point(_ArcX, _ArcY);
+            var _Arc = new ArcSegment
+            {
+                IsLargeArc = Angle >= 180.0,
+                Point = _ArcP,
+                Size = _ArcS,
+                SweepDirection = SweepDirection.Clockwise,
+            };
+            _Figure.Segments.Add(_Arc);
+
+            // finalé
+            Data = new PathGeometry { Figures = { _Figure } };
+            InvalidateArrange();
+        }
+
+    }
+}
---
+++ QuickShare/Controls/CircularProgressBar.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Shapes;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace QuickShare.Controls
+{
+    public sealed class CircularProgressBar : ProgressBar
+    {
+        Arc indicatorArc;
+        TextBlock indicatorPercent;
+        Storyboard storyboard;
+
+        public CircularProgressBar()
+        {
+            this.DefaultStyleKey = typeof(CircularProgressBar);
+            this.ValueChanged += CircularProgressBar_ValueChanged;
+            this.SizeChanged += CircularProgressBar_SizeChanged;
+        }
+
+        double thickness = 2.0;
+        public double Thickness
+        {
+            get
+            {
+                return thickness;
+            }
+            set
+            {
+                thickness = value;
+                SetArcThickness();
+            }
+        }
+
+        Brush stroke = null;
+        public Brush Stroke
+        {
+            get
+            {
+                return stroke;
+            }
+            set
+            {
+                stroke = value;
+                if (indicatorArc != null)
+                    indicatorArc.Stroke = stroke;
+            }
+        }
+
+        Visibility percentIndicatorVisibility;
+        public Visibility PercentIndicatorVisibility
+        {
+            get
+            {
+                return percentIndicatorVisibility;
+            }
+            set
+            {
+                percentIndicatorVisibility = value;
+                if (indicatorPercent != null)
+                    indicatorPercent.Visibility = value;
+            }
+        }
+
+        private void CircularProgressBar_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (indicatorArc == null)
+                return;
+
+            SetArcRadius();
+        }
+
+        private void SetArcRadius()
+        {
+            indicatorArc.Radius = Math.Min(this.Width / 2, this.Height / 2) - thickness;
+        }
+
+        private void CircularProgressBar_ValueChanged(object sender, Windows.UI.Xaml.Controls.Primitives.RangeBaseValueChangedEventArgs e)
+        {
+            if (indicatorArc == null)
+                return;
+
+            UpdateArcValues();
+        }
+
+        private void UpdateArcValues()
+        {
+            storyboard = new Storyboard();
+
+            double _value = Value;
+            if (_value < Minimum)
+                _value = Minimum;
+
+            if (_value >= Maximum)
+                _value = Maximum - (Maximum - Minimum) / (1000.0 * Maximum);
+
+            double currentAngle = indicatorArc.Angle;
+            double newAngle = ((_value - Minimum) / (Maximum - Minimum)) * 360.0;
+
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = currentAngle;
+            da.To = newAngle;
+            da.EasingFunction = new ExponentialEase() { EasingMode = EasingMode.EaseOut };
+            da.Duration = TimeSpan.FromMilliseconds(250);
+            da.EnableDependentAnimation = true;
+
+            Storyboard.SetTarget(da, indicatorArc);
+            Storyboard.SetTargetProperty(da, "Angle");
+
+            storyboard.Children.Add(da);
+            storyboard.Begin();
+
+            if (PercentIndicatorVisibility == Visibility.Visible)
+                indicatorPercent.Text = ((int)Math.Round(((_value - Minimum) / (Maximum - Minimum)) * 100)).ToString() + "%";
+        }
+
+        private void SetArcThickness()
+        {
+            if (indicatorArc == null)
+                return;
+
+            indicatorArc.StrokeThickness = thickness;
+            indicatorArc.Margin = new Thickness(thickness, thickness, 0, 0);
+            SetArcRadius();
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            indicatorArc = this.GetTemplateChild("indicatorArc") as Arc;
+            indicatorPercent = this.GetTemplateChild("indicatorPercent") as TextBlock;
+
+            indicatorPercent.Visibility = PercentIndicatorVisibility;
+
+            UpdateArcValues();
+            SetArcRadius();
+
+            if (stroke != null)
+                indicatorArc.Stroke = Stroke;
+            indicatorArc.StrokeThickness = Thickness;
+
+
+            base.OnApplyTemplate();
+        }
+
+    }
+}
