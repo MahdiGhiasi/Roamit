7aa0dca8a0aa63257d71e9b2f2217801d60e6112
Mahdi Ghiasi <ghiasi@outlook.com>
2017-05-31 14:35:05 +0430

    SecureKeyStorage

---
+++ QuickShare/HelperClasses/SecureKeyStorage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Credentials;
+
+namespace QuickShare.HelperClasses
+{
+    internal static class SecureKeyStorage
+    {
+        internal static bool IsUserIdStored()
+        {
+            var vault = new PasswordVault();
+            var cred = vault.RetrieveAll().FirstOrDefault(x => x.Resource == "userId");
+
+            if (cred == null)
+                return false;
+
+            return true;
+        }
+
+        internal static string GetUserId()
+        {
+            var vault = new PasswordVault();
+            var cred = vault.RetrieveAll().FirstOrDefault(x => x.Resource == "userId");
+
+            if (cred == null)
+                return "";
+
+            cred.RetrievePassword();
+            return cred.Password;
+        }
+
+        internal static void SetUserId(string userId)
+        {
+            var vault = new PasswordVault();
+            var cred = new PasswordCredential("userId", "user", userId);
+            vault.Add(cred);
+        }
+    }
+}
