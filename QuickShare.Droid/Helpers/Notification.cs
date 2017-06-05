dbf57fb54a8dbe8f54d0754066dc383c782e5985
Mahdi Ghiasi <ghiasi@outlook.com>
2017-06-05 04:31:06 +0430

    Notification.cs

---
+++ QuickShare.Droid/Helpers/Notification.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Media;
+using Android.Support.V4.App;
+
+namespace QuickShare.Droid.Helpers
+{
+    internal static class Notification
+    { 
+
+        private static void SendHeadsUpNotification(Context context, string title, string body)
+        {
+            var intent = new Intent(context, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+
+            var pendingIntent = PendingIntent.GetActivity(context, 0, intent, PendingIntentFlags.OneShot);
+
+            var defaultSoundUri = RingtoneManager.GetDefaultUri(RingtoneType.Notification);
+
+            var notificationBuilder =
+                new NotificationCompat.Builder(context)
+                    .SetSmallIcon(Resource.Drawable.Icon)
+                    .SetPriority((int)NotificationPriority.Max)
+                    .SetContentTitle(title)
+                    .SetContentText(body)
+                    .SetAutoCancel(true)
+                    .SetSound(defaultSoundUri)
+                    .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManager.FromContext(context);
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+    }
+}
