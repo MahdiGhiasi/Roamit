e2226c76932e0437865d569e28a8b004d4ec7c1b
Mahdi Ghiasi <ghiasi@outlook.com>
2017-06-06 04:27:03 +0430

    AwaitTimeout (used in AndroidRomePackageManager retry, commited before)

---
+++ Common/AwaitTimeout.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickShare.Common
+{
+    public static class AwaitTimeout
+    {
+        public static Task<TResult> WithTimeout<TResult>(this Task<TResult> task, TimeSpan timeout, TResult failedValue)
+        {
+            var timeoutTask = Task.Delay(timeout).ContinueWith(_ => failedValue, TaskContinuationOptions.ExecuteSynchronously);
+            return Task.WhenAny(task, timeoutTask).Unwrap();
+        }
+
+        public static Task<TResult> WithTimeout<TResult>(this Task<TResult> task, TimeSpan timeout)
+        {
+            var timeoutTask = Task.Delay(timeout).ContinueWith(_ => default(TResult), TaskContinuationOptions.ExecuteSynchronously);
+            return Task.WhenAny(task, timeoutTask).Unwrap();
+        }
+
+        public static Task WithTimeout(this Task task, TimeSpan timeout)
+        {
+            var timeoutTask = Task.Delay(timeout);
+            return Task.WhenAny(task, timeoutTask).Unwrap();
+        }
+    }
+}
