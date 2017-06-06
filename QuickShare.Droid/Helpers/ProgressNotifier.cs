7d4c497a212483d51ea19ccbc01874a3112732c5
Mahdi Ghiasi <ghiasi@outlook.com>
2017-06-06 16:41:53 +0430

    Increased reliability of notifications (but still not enough)

--- QuickShare.Droid/Helpers/ProgressNotifier.cs
+++ QuickShare.Droid/Helpers/ProgressNotifier.cs
@@ -11,15 +11,20 @@ using Android.Views;
 using Android.Widget;
 using Android.Support.V4.App;
 using Android.Media;
+using System.Threading.Tasks;
 
 namespace QuickShare.Droid.Helpers
 {
     internal class ProgressNotifier
     {
+        readonly TimeSpan _minimumTimeBetweenNotifs = TimeSpan.FromSeconds(1);
+
         int id;
         Context context;
         NotificationManager notificationManager;
         NotificationCompat.Builder builder;
+
+        DateTime lastProgressNotif = DateTime.MinValue;
 
         public ProgressNotifier(Context _context)
         {
@@ -33,21 +38,26 @@ namespace QuickShare.Droid.Helpers
         {
             builder = new NotificationCompat.Builder(context)
                 .SetContentTitle(title)
-                .SetContentText(id.ToString() + "  " + text)
+                .SetContentText(text)
                 .SetSmallIcon(Resource.Drawable.Icon)
                 .SetProgress(0, 0, true);
 
             notificationManager.Notify(id, builder.Build());
+            lastProgressNotif = DateTime.Now;
         }
 
         public void SetProgressValue(int max, int value)
         {
+            if ((DateTime.Now - lastProgressNotif) < _minimumTimeBetweenNotifs)
+                return;
+
             int percent = (100 * value) / max;
 
             builder.SetProgress(max, value, false)
-                .SetContentText(id.ToString() + "  " + $"{percent}%");
+                .SetContentText($"{percent}%");
 
             notificationManager.Notify(id, builder.Build());
+            lastProgressNotif = DateTime.Now;
         }
 
         public void MakeIndetermine(string text = "")
@@ -58,12 +68,15 @@ namespace QuickShare.Droid.Helpers
             notificationManager.Notify(id, builder.Build());
         }
 
-        public void FinishProgress(string title, string text)
+        public async void FinishProgress(string title, string text)
         {
+            if ((DateTime.Now - lastProgressNotif) < _minimumTimeBetweenNotifs)
+                await Task.Delay(DateTime.Now - lastProgressNotif);
+
             builder.SetSound(RingtoneManager.GetDefaultUri(RingtoneType.Notification))
                 .SetPriority((int)NotificationPriority.Max)
                 .SetContentTitle(title)
-                .SetContentText(id.ToString() + "  " + text)
+                .SetContentText(text)
                 .SetProgress(0, 0, false);
 
             notificationManager.Notify(id, builder.Build());
