5908bbd0dd0a84a8edf88c86f69bfd61237121a9
Mahdi Ghiasi <ghiasi@outlook.com>
2017-06-26 17:46:26 +0430

    Android phones show with phone icon now + removed a useless Debug.WriteLine

--- RomeComponent/RemoteSystemKindToIconConverter.cs
+++ RomeComponent/RemoteSystemKindToIconConverter.cs
@@ -14,13 +14,14 @@ namespace QuickShare.Rome
             if ((value is string))
             {
                 var system = value as string;
-                System.Diagnostics.Debug.WriteLine(system);
+                
                 switch (system)
                 {
                     case "Xbox":
                         return Char.ConvertFromUtf32(0xE7FC);
                     case "Mobile":
                     case "Phone":
+                    case "QS_Android":
                         return Char.ConvertFromUtf32(0xE8EA);
                     case "Unknown":
                         return Char.ConvertFromUtf32(0xEC64);
