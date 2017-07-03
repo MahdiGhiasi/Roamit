8f732039e8e59eb86340e3f7e525e9d36afdec41
Mahdi Ghiasi <ghiasi@outlook.com>
2017-07-04 03:10:30 +0430

    Desktop: Clipboard tracking + UI stuff

--- QuickShare.Desktop/MainWindow.xaml.cs
+++ QuickShare.Desktop/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuickShare.Desktop.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -23,14 +24,49 @@ namespace QuickShare.Desktop
     public partial class MainWindow : Window
     {
         NotifyIcon notifyIcon = new NotifyIcon();
+        ClipboardManager clipboardManager;
+
+        MainViewModel ViewModel { get; } = new MainViewModel();
 
         public MainWindow()
         {
             InitializeComponent();
+            SetWindowPosition();
 
-            var desktopWorkingArea = System.Windows.SystemParameters.WorkArea;
+            ClipboardActivity.ItemsSource = ViewModel.ClipboardActivities;
+        }
+
+        private void SetWindowPosition()
+        {
+            var desktopWorkingArea = SystemParameters.WorkArea;
             this.Left = desktopWorkingArea.Right - this.Width;
             this.Top = desktopWorkingArea.Bottom - this.Height;
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            // Initialize the clipboard now that we have a window soruce to use
+            clipboardManager = new ClipboardManager(this);
+            clipboardManager.ClipboardChanged += ClipboardChanged;
+        }
+
+        private void ClipboardChanged(object sender, EventArgs e)
+        {
+            // Handle your clipboard update here, debug logging example:
+            if (System.Windows.Clipboard.ContainsText())
+            {
+                string text = System.Windows.Clipboard.GetText();
+
+                ClipboardActivity.Visibility = Visibility.Visible;
+
+                if ((ViewModel.ClipboardActivities.Count > 0) &&
+                    (ViewModel.ClipboardActivities.Last().DisplayText == text))
+                    return;
+
+                ViewModel.ClipboardActivities.Insert(0, new ClipboardItem(text));
+            }
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
