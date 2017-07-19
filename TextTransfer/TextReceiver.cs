923db676fe1aae734a9441d260ff41780bf186d8
Mahdi Ghiasi <ghiasi@outlook.com>
2017-07-19 22:08:59 +0430

    TextReceiver new logic - solves some text corruption issues

--- TextTransfer/TextReceiver.cs
+++ TextTransfer/TextReceiver.cs
@@ -1,8 +1,10 @@
-﻿using QuickShare.DataStore;
+﻿using Newtonsoft.Json;
+using QuickShare.DataStore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace QuickShare.TextTransfer
@@ -12,66 +14,88 @@ namespace QuickShare.TextTransfer
         public delegate void TextReceiveFinishedEventHandler(TextReceiveEventArgs e);
         public static event TextReceiveFinishedEventHandler TextReceiveFinished;
 
+        static SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
+
         public static async Task<bool> ReceiveRequest(Dictionary<string, object> data)
         {
-            var type = (ContentType)data["Type"];
-            Guid guid;
-            if (!Guid.TryParse((string)data["UniqueId"], out guid))
+            await semaphore.WaitAsync();
+
+            try
             {
-                TextReceiveFinished?.Invoke(new TextReceiveEventArgs
-                {
-                    Success = false,
-                    Guid = null,
-                    HostName = "",
-                });
-                return false;
-            }
-            
-            if (type == ContentType.ClipboardContent)
-            {
-                var partNumber = (int)data["PartNumber"];
-                var totalParts = (int)data["TotalParts"];
-
-                if (!DataStorageProviders.TextReceiveContentManager.IsOpened)
-                    await DataStorageProviders.TextReceiveContentManager.OpenAsync();
-
-                if (partNumber == 0)
-                    DataStorageProviders.TextReceiveContentManager.Add(guid, "");
-
-                if (!DataStorageProviders.TextReceiveContentManager.ContainsKey(guid))
+                var type = (ContentType)data["Type"];
+                if (!Guid.TryParse((string)data["UniqueId"], out Guid guid))
                 {
                     TextReceiveFinished?.Invoke(new TextReceiveEventArgs
                     {
                         Success = false,
-                        Guid = guid,
-                        HostName = (string)data["SenderName"],
+                        Guid = null,
+                        HostName = "",
                     });
                     return false;
                 }
 
-                DataStorageProviders.TextReceiveContentManager.Add(guid, DataStorageProviders.TextReceiveContentManager.GetItemContent(guid) + (string)data["Content"]);
-                DataStorageProviders.TextReceiveContentManager.Close();
-
-                if (partNumber == (totalParts - 1)) //Finished receiving data.
+                if (type == ContentType.ClipboardContent)
                 {
-                    await DataStorageProviders.HistoryManager.OpenAsync();
-                    DataStorageProviders.HistoryManager.Add(guid, 
-                        DateTime.Now,
-                        (string)data["SenderName"],
-                        new ReceivedText(),
-                        true);
-                    DataStorageProviders.HistoryManager.Close();
+                    var partNumber = (int)data["PartNumber"];
+                    var totalParts = (int)data["TotalParts"];
 
-                    TextReceiveFinished?.Invoke(new TextReceiveEventArgs
+                    if (!DataStorageProviders.TextReceiveContentManager.IsOpened)
+                        await DataStorageProviders.TextReceiveContentManager.OpenAsync();
+
+                    if (!DataStorageProviders.TextReceiveContentManager.ContainsKey(guid))
+                        DataStorageProviders.TextReceiveContentManager.Add(guid, JsonConvert.SerializeObject(new Dictionary<int, string>()));
+
+                    Dictionary<int, string> parts = JsonConvert.DeserializeObject<Dictionary<int, string>>(DataStorageProviders.TextReceiveContentManager.GetItemContent(guid));
+
+                    parts[partNumber] = (string)data["Content"];
+
+                    DataStorageProviders.TextReceiveContentManager.Add(guid, JsonConvert.SerializeObject(parts));
+
+                    if (parts.Count != totalParts)
                     {
-                        Success = true,
-                        Guid = guid,
-                        HostName = (string)data["SenderName"],
-                    });
+                        DataStorageProviders.TextReceiveContentManager.Close();
+                    }
+                    else //Finished receiving data.
+                    {
+                        string finalString = "";
+                        for (int i = 0; i < totalParts; i++)
+                        {
+                            finalString += parts[i];
+                        }
+                        DataStorageProviders.TextReceiveContentManager.Add(guid, finalString);
+                        DataStorageProviders.TextReceiveContentManager.Close();
+
+                        await DataStorageProviders.HistoryManager.OpenAsync();
+                        DataStorageProviders.HistoryManager.Add(guid,
+                            DateTime.Now,
+                            (string)data["SenderName"],
+                            new ReceivedText(),
+                            true);
+                        DataStorageProviders.HistoryManager.Close();
+
+                        TextReceiveFinished?.Invoke(new TextReceiveEventArgs
+                        {
+                            Success = true,
+                            Guid = guid,
+                            HostName = (string)data["SenderName"],
+                        });
+                    }
+
                 }
+
+                return true;
             }
-            
-            return true;
+            catch
+            {
+                if (semaphore.CurrentCount == 0)
+                    semaphore.Release();
+                throw;
+            }
+            finally
+            {
+                if (semaphore.CurrentCount == 0)
+                    semaphore.Release();
+            }
         }
 
         public static async Task<Guid> QuickTextReceivedAsync(string sender, string text)
