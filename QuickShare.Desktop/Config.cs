b34a75f483911ef21b46459b60484766a41dc488
Mahdi Ghiasi <ghiasi@outlook.com>
2017-07-28 00:33:21 +0430

    desktop app: improvements

---
+++ QuickShare.Desktop/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickShare.Desktop
+{
+    internal static class Config
+    {
+        internal static readonly string ServerAddress = "http://localhost:14100";
+    }
+}
