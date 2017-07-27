b7f85a95afc2013ee1722d03fa6bd6dfb63a285f
Mahdi Ghiasi <ghiasi@outlook.com>
2017-07-28 01:02:23 +0430

    Desktop app: Register to run on startup

---
+++ QuickShare.Desktop/StartupManager.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickShare.Desktop
+{
+    // from http://www.visualbx.com/blog/run-wpf-application-at-windows-startup/
+    public class StartupManager
+    {
+        string appName;
+
+        public StartupManager(string _appName)
+        {
+            appName = _appName;
+        }
+
+        public void AddApplicationToCurrentUserStartup()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                key.SetValue(appName, "\"" + System.Reflection.Assembly.GetExecutingAssembly().Location + "\"");
+            }
+        }
+
+        public void AddApplicationToAllUserStartup()
+        {
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                key.SetValue(appName, "\"" + System.Reflection.Assembly.GetExecutingAssembly().Location + "\"");
+            }
+        }
+
+        public void RemoveApplicationFromCurrentUserStartup()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                key.DeleteValue(appName, false);
+            }
+        }
+
+        public void RemoveApplicationFromAllUserStartup()
+        {
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+            {
+                key.DeleteValue(appName, false);
+            }
+        }
+
+        public bool IsUserAdministrator()
+        {
+            //bool value to hold our return value
+            bool isAdmin;
+            try
+            {
+                //get the currently logged in user
+                WindowsIdentity user = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                isAdmin = false;
+            }
+            catch (Exception ex)
+            {
+                isAdmin = false;
+            }
+            return isAdmin;
+        }
+    }
+}
