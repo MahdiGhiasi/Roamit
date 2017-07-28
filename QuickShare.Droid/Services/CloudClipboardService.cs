17ba0546c636f3c3c14e954c5ba7302d81789604
Mahdi Ghiasi <ghiasi@outlook.com>
2017-07-28 16:43:45 +0430

    Droid: Cloud Clipboard

---
+++ QuickShare.Droid/Services/CloudClipboardService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Preferences;
+using Android.Util;
+
+namespace QuickShare.Droid.Services
+{
+    [Service]
+    internal class CloudClipboardService : IntentService
+    {
+        public CloudClipboardService() :
+            base("CloudClipboardService")
+        {
+
+        }
+        protected override void OnHandleIntent(Intent intent)
+        {
+            if (intent.Action == "CloudClipboardCopy")
+            {
+                ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(this);
+                var text = prefs.GetString("CloudClipboardText", "");
+
+                Handler handler = new Handler(Looper.MainLooper);
+                handler.Post(() =>
+                {
+                    try
+                    {
+                        ClipboardManager clipboard = (ClipboardManager)GetSystemService(Context.ClipboardService);
+                        ClipData clip = ClipData.NewPlainText(text, text);
+                        clipboard.PrimaryClip = clip;
+
+                        Toast.MakeText(this, "Copied", ToastLength.Short).Show();
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Debug("CloudClipboardService", ex.Message);
+                    }
+                });
+            }
+        }
+    }
+}
