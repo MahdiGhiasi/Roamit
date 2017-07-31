362ca3ef0ae9aa50365254106afbec279bb34773
Mahdi Ghiasi <ghiasi@outlook.com>
2017-07-31 19:59:57 +0430

    Desktop app: too lazy to write commit description

--- QuickShare.Desktop/SettingsWindow.xaml.cs
+++ QuickShare.Desktop/SettingsWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using QuickShare.Desktop.Helpers;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,13 +33,24 @@ namespace QuickShare.Desktop
 
         private void SignOutAndExit_Click(object sender, RoutedEventArgs e)
         {
-            var result = MessageBox.Show("Are you sure you want to sign out?\r\n\r\nYour clipboard content will not be synced to your devices. If you want to enable Cloud Clipboard again, you'll need to open 'Roamit Cloud Clipboard' from start menu and sign in with your Microsoft account.",
+            var result = MessageBox.Show("Are you sure you want to sign out?\r\n\r\nYour clipboard content will not be synced to your devices, and the extension will not run on startup anymore.\r\nIf you want to enable Cloud Clipboard again, you'll need to open 'Roamit Cloud Clipboard' from start menu and sign in with your Microsoft account.",
                 "Roamit", MessageBoxButton.YesNo, MessageBoxImage.Exclamation, MessageBoxResult.No);
 
             if (result == MessageBoxResult.Yes)
             {
                 Properties.Settings.Default.AccountId = "";
                 Properties.Settings.Default.Save();
+
+                try
+                {
+                    var startupManager = new StartupManager("Roamit Cloud Clipboard");
+                    startupManager.RemoveApplicationFromCurrentUserStartup();
+                }
+                catch
+                {
+                    Debug.WriteLine("Failed to register program to run at startup.");
+                }
+
                 Application.Current.Shutdown();
             }
         }
