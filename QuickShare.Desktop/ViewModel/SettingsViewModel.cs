37e7bf8c4202c6aa0f38cd38fd3bff8703fbfe9a
Mahdi Ghiasi <ghiasi@outlook.com>
2017-08-01 19:26:03 +0430

    Desktop app

---
+++ QuickShare.Desktop/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickShare.Desktop.ViewModel
+{
+    public class SettingsViewModel : INotifyPropertyChanged
+    {
+        public SettingsViewModel()
+        {
+
+        }
+
+        public string VersionNumber
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public ObservableCollection<DeviceItem> Devices { get; } = new ObservableCollection<DeviceItem>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Create the OnPropertyChanged method to raise the event
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class DeviceItem
+    {
+        public string Name { get; set; }
+        public DeviceType Type { get; set; }
+
+        private bool isActive;
+        public bool IsActive
+        {
+            get
+            {
+                return isActive;
+            }
+            set
+            {
+                isActive = value;
+                //TODO: Save changes
+            }
+        }
+    }
+
+    public enum DeviceType
+    {
+        PC,
+        Phone,
+    }
+}
