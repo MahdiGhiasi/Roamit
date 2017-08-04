0a176dc78a37384faf80342e0ad78a846737e2c6
Mahdi Ghiasi <ghiasi@outlook.com>
2017-08-04 19:11:50 +0430

    Desktop app: Single Instance + Some Squirrel updater  code

---
+++ QuickShare.Desktop/Helpers/Updater.cs
@@ -0,0 +1,31 @@
+﻿using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickShare.Desktop.Helpers
+{
+    internal static class Updater
+    {
+        internal static async Task CheckForUpdates()
+        {
+            using (var mgr = new UpdateManager(@"C:\Users\Mahdi\Projects\QuickShare\Releases"))
+            {
+                Debug.WriteLine("Checking for updates...");
+
+                var updateInfo = await mgr.CheckForUpdate();
+                if (updateInfo.ReleasesToApply.Any())
+                {
+                    Debug.WriteLine($"Update available. Downloading and installing...");
+                    await mgr.UpdateApp();
+
+                    Debug.WriteLine("Restarting app...");
+                    UpdateManager.RestartApp();
+                }
+            }
+        }
+    }
+}
