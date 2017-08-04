6290fb262f851d0ec0cd45ed1c1b3ea78e0359f7
Mahdi Ghiasi <ghiasi@outlook.com>
2017-08-04 19:11:50 +0430

    Desktop app: Single Instance + Some Squirrel updater  code

--- QuickShare.Desktop/App.xaml.cs
+++ QuickShare.Desktop/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Shell;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -11,7 +12,36 @@ namespace QuickShare.Desktop
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application , ISingleInstanceApp
     {
+        private const string Unique = "RoamitDesktopExtension";
+
+        // Single instance code from http://blogs.microsoft.co.il/arik/2010/05/28/wpf-single-instance-application/
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+            {
+                var application = new App();
+
+                application.InitializeComponent();
+                application.Run();
+
+                // Allow single instance code to perform cleanup operations
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        #region ISingleInstanceApp Members
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            // handle command line arguments of second instance
+            // …
+
+            return true;
+        }
+
+        #endregion
     }
 }
