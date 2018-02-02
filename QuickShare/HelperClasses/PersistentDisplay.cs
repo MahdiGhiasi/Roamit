20f3596239cff1b20391d9ded90aa36c3b82467d
Mahdi <mghiasi75@gmail.com>
2018-02-02 14:46:56 +0330

    UWP: Don't throw an exception if Activate/ReleasePersistentDisplay fails.

--- QuickShare/HelperClasses/PersistentDisplay.cs
+++ QuickShare/HelperClasses/PersistentDisplay.cs
@@ -13,26 +13,34 @@ namespace QuickShare.HelperClasses
 
         public static void ActivatePersistentDisplay()
         {
-            //create the request instance if needed
-            if (_displayRequest == null)
-                _displayRequest = new Windows.System.Display.DisplayRequest();
-            else if (isAlreadyRequested)
-                return;
+            try
+            {
+                //create the request instance if needed
+                if (_displayRequest == null)
+                    _displayRequest = new Windows.System.Display.DisplayRequest();
+                else if (isAlreadyRequested)
+                    return;
 
-            //make request to put in active state
-            _displayRequest.RequestActive();
-            isAlreadyRequested = true;
+                //make request to put in active state
+                _displayRequest.RequestActive();
+                isAlreadyRequested = true;
+            }
+            catch { }
         }
 
         public static void ReleasePersistentDisplay()
         {
-            //must be same instance, so quit if it doesn't exist
-            if (_displayRequest == null)
-                return;
+            try
+            {
+                //must be same instance, so quit if it doesn't exist
+                if (_displayRequest == null)
+                    return;
 
-            //undo the request
-            _displayRequest.RequestRelease();
-            isAlreadyRequested = false;
+                //undo the request
+                _displayRequest.RequestRelease();
+                isAlreadyRequested = false;
+            }
+            catch { }
         }
     }
 }
