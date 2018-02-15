a2d2e6ed3bba50cbd586e7bc471e8cd0634338a9
Mahdi <mghiasi75@gmail.com>
2018-02-15 17:49:15 +0330

    UWP: Increased version to 2.7.0.0 + updated what's new page

--- QuickShare/HelperClasses/WhatsNewHelper.cs
+++ QuickShare/HelperClasses/WhatsNewHelper.cs
@@ -43,6 +43,9 @@ namespace QuickShare.HelperClasses
             if (ApplicationData.Current.LocalSettings.Values.ContainsKey("LatestWhatsNewVersion"))
                 System.Version.TryParse(ApplicationData.Current.LocalSettings.Values["LatestWhatsNewVersion"].ToString(), out prevVersion);
 
+            if (prevVersion < new System.Version("2.7.0.0"))
+                output.Add("10");
+
             if (prevVersion < new System.Version("2.6.0.0"))
                 output.Add("9");
 
